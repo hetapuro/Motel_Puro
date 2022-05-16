a0f99a967fff1ff1ce7847927562ea541a8b2d81
hetapuro <dhepu002@edu.xamk.fi>
2022-05-16 14:59:57 +0300

    Frontend buttons and input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [lodgings, setLodgings] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  let [adults, setAdults] = useState(1)
+  let [children, setChildren] = useState(0)
+
+  function minusA() {
+    if (adults > 1) {
+      adults = adults - 1
+      setAdults(adults)
+    }
+  }
+
+  function plusA() {
+    adults = adults + 1
+    setAdults(adults)
+  }
+
+  function minusC() {
+    if (children > 0) {
+      children = children - 1
+      setChildren(children)
+    }
+  }
+
+  function plusC() {
+    children = children + 1
+    setChildren(children)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Majoittaudu</h2>
+      <form>
+        <div>
+          <label for='arrival'>Saapumispäivä:</label> 
+          <br/>
+          <input name='arrival' type='date'/>
+          <br/>
+        </div>
+        <div>
+          <button type='button' id='minusA' onClick={minusA}>-</button>
+          <span id='adults'>{adults}</span>
+          <button type='button' id='plusA' onClick={plusA}>+</button>
+        </div>
+        <div>
+          <button type='button' id='minusC' onClick={minusC}>-</button>
+          <span id='children'>{children}</span>
+          <button type='button' id='plusC' onClick={plusC}>+</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
