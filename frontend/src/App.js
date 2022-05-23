b070db16dfd4f10dc735d74f2ee9946d01bff2ec
hetapuro <dhepu002@edu.xamk.fi>
2022-05-23 10:39:42 +0300

    mm. CSS
    En saanu oikein mitään tehtyä. vähän pyörittelin CSS ja yritin miettiä, kuinka tallentaa asioita, mut ei oikein onnistunu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,60 @@
 import { useState } from 'react'
+import './App.css'
 
 const App = () => {
-  const [lodgings, setLodgings] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  let [adults, setAdults] = useState(1)
-  let [children, setChildren] = useState(0)
+  const [adults, setAdults] = useState(1)
+  const [children, setChildren] = useState(0)
 
-  function minusA() {
+  const minusA = () => {
     if (adults > 1) {
-      adults = adults - 1
-      setAdults(adults)
+      setAdults(adults - 1)
     }
   }
 
-  function plusA() {
-    adults = adults + 1
-    setAdults(adults)
+  const plusA = () => {
+    setAdults(adults + 1)
   }
 
-  function minusC() {
+  const minusC = () => {
     if (children > 0) {
-      children = children - 1
-      setChildren(children)
+      setChildren(children - 1)
     }
   }
 
-  function plusC() {
-    children = children + 1
-    setChildren(children)
+  const plusC = () => {
+    setChildren(children + 1)
   }
+
+  var people = adults + children
 
   return (
-    <div>
-      <h2>Majoittaudu</h2>
-      <form>
-        <div>
-          <label for='arrival'>Saapumispäivä:</label> 
-          <br/>
-          <input name='arrival' type='date'/>
-          <br/>
-        </div>
-        <div>
-          <button type='button' id='minusA' onClick={minusA}>-</button>
-          <span id='adults'>{adults}</span>
-          <button type='button' id='plusA' onClick={plusA}>+</button>
-        </div>
-        <div>
-          <button type='button' id='minusC' onClick={minusC}>-</button>
-          <span id='children'>{children}</span>
-          <button type='button' id='plusC' onClick={plusC}>+</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
+    <div id='wrapper'>
+      <div id='header'>
+        <h1>Le Motel de Puro</h1>
+      </div>
+      <div>
+        <h2>Majoittaudu</h2>
+        <form>
+            <label>Saapumispäivä:</label> <br/>
+            <input type='date'/>
+
+            <label>Aikuisia:</label> <br/>
+            <button type='button' onClick={minusA}>-</button>
+            <span>{adults}</span>
+            <button type='button' onClick={plusA}>+</button> <br/>
+
+            <label>Lapsia: </label> <br/>
+            <button type='button' onClick={minusC}>-</button>
+            <span>{children}</span>
+            <button type='button' onClick={plusC}>+</button>
+            <p>Henkilöiden määrä: {people}</p>
+  
+          <button type='submit'>Kirjaa majoittautuminen</button>
+        </form>
+      </div>
+      <div>
+        <h2>Majoittautumiset</h2>
+      </div>
     </div>
   )
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+const lodgings = [
+    {
+        id: 1,
+        arrival: '2019-05-30T17:30:31.098Z',
+        adults: 2,
+        children: 3
+    },
+    {
+        id: 2,
+        arrival: '2019-05-30T19:20:14.298Z',
+        adults: 4,
+        children: 0
+    }
+]
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
